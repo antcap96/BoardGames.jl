951f8745e7e43da55c7181bb04c8687dda4c8578
António Capela <acapela96@hotmail.com>
2020-07-30 00:35:49 +0100

    support for more than 2 player (untested)

--- src/strategy.jl
+++ src/strategy.jl
@@ -9,22 +9,17 @@ function getmove end
     playgame(g::Game, s1::Strategy, s2::Strategy)
     playgame(board, s1::Strategy, s2::Strategy)
 """
+
 #TODO: decide output
-function playgame(board, s1::Strategy, s2::Strategy)
+function playgame(board, s::Vararg{Strategy})
 
     while !isempty(getmoves(board))
         p = playerturn(board)
-
-        if p == 1
-            m = getmove(board, s1)
-        else #if p == 2
-            m = getmove(board, s2)
-        end
-
+        m = getmove(board, s[p])
         board = play(board, m)
     end
 
     return board#TODO
 end
 
-playgame(g::Game, s1::Strategy, s2::Strategy) = playgame(initialboard(g), s1, s2)
+playgame(g::Game, s::Vararg{Strategy}) = playgame(initialboard(g), s...)
